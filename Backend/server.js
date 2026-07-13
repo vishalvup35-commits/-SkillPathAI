d1efbf20eb2b6e1a1fac1cbaadc51145ea7af1d4
vishalvup35-commits <vishalvup35@gmail.com>
2026-07-13 19:16:48 +0530

    Add express-rate-limit

--- Backend/server.js
+++ Backend/server.js
@@ -83,7 +83,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
